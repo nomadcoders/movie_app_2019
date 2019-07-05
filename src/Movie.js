c0a3270f5824c2555e2621190c6307cbaefe0704
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-07-05 18:46:28 +0900

    #4.4 Styles Timelapse

--- src/Movie.js
+++ src/Movie.js
@@ -9,7 +9,7 @@ function Movie({ year, title, summary, poster, genres }) {
       <div className="movie__data">
         <h3 className="movie__title">{title}</h3>
         <h5 className="movie__year">{year}</h5>
-        <ul className="genres">
+        <ul className="movie__genres">
           {genres.map((genre, index) => (
             <li key={index} className="genres__genre">
               {genre}
