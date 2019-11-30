b01f6914e9babb034f6fbd58e178b3171773b136
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-11-30 15:54:55 +0900

    #6.1 Building the Router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { HashRouter, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
 
 function App() {
-  return <span>something</span>;
+  return (
+    <HashRouter>
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+    </HashRouter>
+  );
 }
 
 export default App;
---
+++ src/routes/About.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function About() {
+  return <span>About this page: I built it because I love movies.</span>;
+}
+
+export default About;
